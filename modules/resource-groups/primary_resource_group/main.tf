60f6b0198798c4f94f41b9f8c6ec0b50b29529c1
ShaankariVoruganti <67142384+ShaankariVoruganti@users.noreply.github.com>
2023-12-04 11:29:48 +0530

    Update main.tf

--- modules/resource-groups/primary_resource_group/main.tf
+++ modules/resource-groups/primary_resource_group/main.tf
@@ -2,5 +2,6 @@
 resource "azurerm_resource_group" "primary_rg" {
   name     = var.primary_resource_group_name
   location = var.primary_location
+   prevent_deletion_if_contains_resources = var.prevent_dlt
 }
 
