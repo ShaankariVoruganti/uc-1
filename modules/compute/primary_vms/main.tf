3fa29d749d4302bf7751837791da0d6069178344
ShaankariVoruganti <67142384+ShaankariVoruganti@users.noreply.github.com>
2023-12-05 11:00:11 +0530

    Update main.tf

--- modules/compute/primary_vms/main.tf
+++ modules/compute/primary_vms/main.tf
@@ -38,8 +38,8 @@ resource "azurerm_mssql_virtual_machine" "sqlvm" {
   sql_connectivity_type=var.sql_conn_type
   sql_connectivity_update_password=var.sql1_password
   sql_connectivity_update_username=var.sql1_username
-timeouts{
-create= "20m"
-delete= "20m"
-}
+//timeouts{
+//create= "20m"
+//delete= "20m"
+//}
 }
